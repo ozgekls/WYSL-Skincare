3972b232a5db803cce41a5db801b2da43d9d49e2
özge keleş <232201002@ogrenci.amasya.edu.tr>
2026-06-18 00:42:54 +0300

    düzeltme

--- skincare_app/lib/data/services/api_service.dart
+++ skincare_app/lib/data/services/api_service.dart
@@ -6,7 +6,7 @@ class ApiService {
   // ÖNEMLİ NOT:
   // Windows masaüstü / Web: 'http://127.0.0.1:8000'
   // Android Emülatör      : 'http://10.0.2.2:8000'
-  //telefon bağlantısı: http://192.168.1.177:8000
+
   static const String baseUrl = 'http://127.0.0.1:8000';
 
   // --- ÜRÜN ANALİZİ (ID ile) ---
